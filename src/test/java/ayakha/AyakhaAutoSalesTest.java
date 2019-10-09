5de34e78924d2f06e776379326c4540dbe4ba9c2
avermeulen <andre.vermeulen@gmail.com>
2019-10-09 13:14:49 +0200

    moved to a cars csv file

--- src/test/java/ayakha/AyakhaAutoSalesTest.java
+++ src/test/java/ayakha/AyakhaAutoSalesTest.java
@@ -1,53 +1,47 @@
 package ayakha;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ResourceBundle;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
-import org.junit.jupiter.api.Test;
-import static ayakha.AyakhaAutoSales.*;
+import static ayakha.AyakhaAutoSales.mostPopularCarModel;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AyakhaAutoSalesTest {
 
-    private static ArrayList<Car> carList() throws IOException, ParseException, FileNotFoundException {
-        JSONParser parser = new JSONParser();
+    private static ArrayList<Car> carList() throws Exception {
 
-        Path carsPath = Paths.get("cars.json");
-        Object obj = parser.parse(new FileReader(carsPath.toFile()));
-        JSONArray objects = (JSONArray) obj;
+        Path carsPath = Paths.get("cars.csv");
+        FileReader fileReader = new FileReader(carsPath.toFile());
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-        Iterator<JSONObject> itr = objects.iterator();
-
+        bufferedReader.readLine();
         ArrayList<Car> cars = new ArrayList<>();
 
-        while(itr.hasNext()) {
-            JSONObject carObj = itr.next();
+        while (true) {
+            String line = bufferedReader.readLine();
+            if (line == null){
+                break;
+            }
 
+            String[] parts = line.split(",");
             Car car = new Car(
-                (String)carObj.get("color"),
-                (String)carObj.get("make"),
-                (String)carObj.get("model"),
-                (String)carObj.get("reg_number"));
+                    parts[0],
+                    parts[1],
+                    parts[2],
+                    parts[3]);
             cars.add(car);
-
         }
         return cars;
     }
 
-	@Test
+	@org.junit.jupiter.api.Test
     public void shouldFindMostPopularCarModel() throws Exception {
+
+        System.out.println(carList().get(0));
+
         assertEquals("", mostPopularCarModel(carList()));
     }
 
